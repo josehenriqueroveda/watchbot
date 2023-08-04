eadf572e082afb54f3a24a60e6cece62b435c74d
Jose Henrique Roveda <jroveda@lpht.com>
2023-08-04 10:22:52 -0300

    Automatic check

--- api_watchbot/main.py
+++ api_watchbot/main.py
@@ -1,7 +1,9 @@
 import os
 import requests
+from typing import Dict, Tuple
 
 import telebot
+from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -12,45 +14,58 @@ TELEGRAM_GROUP_ID = os.environ["TELEGRAM_GROUP_ID"]
 API_LIST = [
     {"name": os.environ["API_NAME_1"], "url": os.environ["API_URL_1"]},
     {"name": os.environ["API_NAME_2"], "url": os.environ["API_URL_2"]},
-    {"name": os.environ["API_NAME_2"], "url": os.environ["API_URL_3"]},
 ]
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+last_warnings: Dict[str, int] = {}
 
 
-def check_api_status(api):
+def check_api_status(api: Dict[str, str]) -> Tuple[int, str]:
     try:
         response = requests.get(api["url"])
-        if response.status_code == 200:
-            return (200, "OK")
+        if response.status_code == requests.codes.ok:
+            return (response.status_code, "OK")
         else:
             return (response.status_code, response.text)
     except requests.exceptions.RequestException as e:
-        return (500, str(e))
+        return (requests.codes.internal_server_error, str(e))
 
 
 @bot.message_handler(commands=["start"])
-def send_welcome(message):
+def send_welcome(message: telebot.types.Message) -> None:
     bot.reply_to(
         message, "Welcome to API WatchBot!\n\n Type /status to check APIs status."
     )
 
 
 @bot.message_handler(commands=["status"])
-def send_status(_):
+def send_status(message: telebot.types.Message) -> None:
     response = "*🤖 API Status Monitoring*\n\n"
     for api in API_LIST:
         status_code, status_text = check_api_status(api)
-        if status_code == 200:
-            response = (
-                response + f"{api['name']}: 🟢 _{status_code} - {status_text}_\n\n"
-            )
+        if status_code == requests.codes.ok:
+            response += f"🟢 {api['name']}: _{status_code} - {status_text}_\n\n"
         else:
-            response = (
-                response + f"{api['name']}: 🔴 _{status_code} - {status_text}_\n\n"
-            )
+            response += f"🔴 {api['name']}: _{status_code} - {status_text}_\n\n"
     bot.send_message(TELEGRAM_GROUP_ID, response, parse_mode="Markdown")
 
 
+def notify_problem() -> None:
+    global last_warnings
+    for api in API_LIST:
+        status_code, status_text = check_api_status(api)
+        if status_code != requests.codes.ok:
+            if last_warnings.get(api["name"]) != status_code:
+                notification = (
+                    f"🔴 *{api['name']}* is down! _{status_code} - {status_text}_"
+                )
+                bot.send_message(TELEGRAM_GROUP_ID, notification, parse_mode="Markdown")
+                last_warnings[api["name"]] = status_code
+
+
 if __name__ == "__main__":
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(notify_problem, "interval", minutes=15)
+    scheduler.start()
+
     bot.infinity_polling()
